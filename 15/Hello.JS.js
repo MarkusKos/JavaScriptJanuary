9f5b24abf56bf0e86da6b5b298fe7c75f4b5d3a9
konstantin <>
2020-03-05 20:53:02 +0300

    Добавлен первый пример node.js

---
+++ 15/Hello.JS.js
@@ -0,0 +1,28 @@
+"use strict";
+const fs = require("fs");
+
+const os = require("os");
+
+const rand = Math.floor(Math.random()*3);
+let text = "";
+
+if (rand === 0){
+    text = "Hello World";
+}else if (rand === 1){
+    text = "Hey there!";
+}else if (rand === 2){
+    text = "What's up?";
+}
+
+console.log(text);
+
+const content = fs.readFileSync("./test.txt");
+
+fs.writeFileSync("./test.txt", content + "\n" + text);
+
+const data = {text,createdAt: new Date()};
+fs.writeFileSync("./test.json", JSON.stringify(data));
+
+fs.writeFileSync("./cpu-info.json", JSON.stringify(os.cpus()));
+
+console.log(os.cpus());
